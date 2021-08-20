4f1a0d0fb6958883e13f7780bafb7a8b21356095
mdmasrur3426 <mdmasrur3426@gmail.com>
2021-08-20 00:02:01 -0400

    day1_ass

---
+++ day_1/helloworld.py
@@ -0,0 +1,33 @@
+Single = 'Hello'
+Sentence = "Hello from Masrur"
+Multiline = """Hello from
+Masrur"""
+
+print("****************SINGLE LINE***********")
+print(Single)
+print("**************************************")
+print('\n')
+
+
+print("*****************SENTENCE*************")
+print(Sentence)
+print("**************************************")
+print('\n')
+
+
+print("*****************MULTI LINE***********")
+print(Multiline)
+print("**************************************")
+print('\n')
+
+
+print("***************ALTERNATIVE************")
+print("Hello from")
+print("Masrur")
+print("**************************************")
+print('\n')
+
+
+print("***************ZULFA's FAV************")
+print("BHAT")
+print("**************************************")
