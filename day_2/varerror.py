ad2c6a9bca940dc2b413b27f6a16d2e4c93c96a7
mdmasrur3426 <mdmasrur3426@gmail.com>
2021-08-21 17:41:37 -0400

    day2_ass

---
+++ day_2/varerror.py
@@ -0,0 +1,35 @@
+var1 =
+
+print(var1)
+
+# OUTPUT
+# C:\Users\rman\Anaconda3\python.exe C:/Users/rman/Desktop/git/Learning_Python_Zulu/day_2/varerror.py
+#   File "C:/Users/rman/Desktop/git/Learning_Python_Zulu/day_2/varerror.py", line 1
+#     var =
+#         ^
+# SyntaxError: invalid syntax
+#
+# Process finished with exit code 1
+
+var2 = null
+
+print(var2)
+
+# OUTPUT
+# C:\Users\rman\Anaconda3\python.exe C:/Users/rman/Desktop/git/Learning_Python_Zulu/day_2/varerror.py
+# Traceback (most recent call last):
+#   File "C:/Users/rman/Desktop/git/Learning_Python_Zulu/day_2/varerror.py", line 14, in <module>
+#     var2 = null
+# NameError: name 'null' is not defined
+#
+# Process finished with exit code 1
+
+var3 = 'null'
+
+print(var3)
+
+# OUTPUT
+# C:\Users\rman\Anaconda3\python.exe C:/Users/rman/Desktop/git/Learning_Python_Zulu/day_2/varerror.py
+# null
+#
+# Process finished with exit code 0
