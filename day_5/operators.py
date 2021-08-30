b07f17d967c3bf74d35b3fe8a6004ddd143e4588
mdmasrur3426 <mdmasrur3426@gmail.com>
2021-08-29 21:15:56 -0400

    day_5 ass

---
+++ day_5/operators.py
@@ -0,0 +1,69 @@
+a = int(input("a = "))
+b = int(input("b = "))
+
+c = a + b
+d = a - b
+e = a / b
+f = a * b
+g = a % b
+h = a // b
+
+
+print("*********Arithmatic Operators****************")
+print("*******Formula: c = a + b *********")
+print(c)
+print("Type is:", type(c))
+
+print("*******Formula: d = a - b *********")
+print(d)
+print("Type is:", type(d))
+
+print("*******Formula: e = a / b *********")
+print(e)
+print("Type is:", type(e))
+
+print("*******Formula: f = a * b *********")
+print(f)
+print("Type is:", type(f))
+
+print("*******Formula: g = a % b *********")
+print(g)
+print("Type is:", type(g))
+
+print("*******Formula: h = a // b *********")
+print(h)
+print("Type is:", type(h))
+
+print("*********Comparison Operator****************")
+print (a == b)
+print(type(a == b))
+print(a !=b)
+print(type(a !=b))
+print(a > b)
+print(type(a > b))
+print(a < b)
+print(type(a < b))
+print(a >= b)
+print(type(a >= b))
+print(a <= b)
+print(type(a <= b))
+
+print("*********Assignment Operators****************")
+print("******** c += a **************")
+c += a
+print(c)
+print("******** d -= a **************")
+d -= a
+print(d)
+print("********  e /= a **************")
+e /= a
+print(e)
+print("******** f *= a **************")
+f *= a
+print(f)
+print("******** g %= a **************")
+g %= a
+print(g)
+print("******** h **= a **************")
+h **= a
+print(c)
